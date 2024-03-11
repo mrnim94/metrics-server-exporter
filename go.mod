5351da4b0d39d004cd4e654a113d8fdcd14882b7
mr nim <mrnim@nimtechnology.com>
2024-03-11 14:30:50 +0700

    implement metrics server

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/labstack/gommon v0.4.2
 	github.com/mrnim94/file-rotatelogs v2.4.0+incompatible
-	github.com/prometheus/client_golang v1.19.0
+	github.com/prometheus/client_golang v1.18.0
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.9.3
 	k8s.io/apimachinery v0.28.4
@@ -45,22 +45,22 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/oauth2 v0.16.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
-	golang.org/x/term v0.16.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231012201019-e917dd12ba7a // indirect
 	google.golang.org/grpc v1.58.3 // indirect
-	google.golang.org/protobuf v1.32.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ helper/kubernetes/kubernetes_impl/hpa_impl.go
@@ -0,0 +1,91 @@
+package kubernetes_impl
+
+import (
+	"context"
+	autoscalingv2 "k8s.io/api/autoscaling/v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"metrics-server-exporter/log"
+	"metrics-server-exporter/model"
+)
+
+func (kc *KubeConfiguration) CurrentMetricsForHPAs(namespace string) (model.HPAUtilizations, error) {
+
+	results := model.HPAUtilizations{}
+
+	// Load kubeconfig
+	config, err := kc.accessKubernetes()
+	if err != nil {
+		log.Error(err)
+		return results, err
+	}
+
+	// Create clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Error(err)
+		return results, err
+	}
+	hpaList, err := clientset.AutoscalingV2().HorizontalPodAutoscalers(namespace).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		log.Error("Error fetching HPAs: %v", err)
+		return results, err
+	}
+	for _, hpa := range hpaList.Items {
+		log.Debugf("HPA: %s", hpa.Name)
+		log.Debugf("ScaleTargetRef: Kind=%s, Name=%s", hpa.Spec.ScaleTargetRef.Kind, hpa.Spec.ScaleTargetRef.Name)
+
+		// Display current metrics for this HPA
+		for _, metric := range hpa.Status.CurrentMetrics {
+			switch metric.Type {
+			case autoscalingv2.ResourceMetricSourceType:
+				log.Debugf("  Resource Metric: %s, Current Average Utilization: %d, Current Average Value: %s",
+					metric.Resource.Name, *metric.Resource.Current.AverageUtilization, metric.Resource.Current.AverageValue.String())
+				results = append(results, model.HPAUtilization{
+					MetricName:         string(metric.Resource.Name),
+					MetricType:         string(metric.Type),
+					HPAOwner:           hpa.Name,
+					ScaleTargetRefKind: hpa.Spec.ScaleTargetRef.Kind,
+					ScaleTargetRefName: hpa.Spec.ScaleTargetRef.Name,
+					AverageUtilization: float64(*metric.Resource.Current.AverageUtilization),
+				})
+
+			case autoscalingv2.ExternalMetricSourceType:
+				var correctNum int
+				for iMetricOfSpec, metricOfSpec := range hpa.Spec.Metrics {
+					if metricOfSpec.External.Metric.Name == metric.External.Metric.Name {
+						correctNum = iMetricOfSpec
+						break
+					}
+				}
+				log.Debugf("  External Metric: %s, Define Average Value: %s, Current Average Value: %s",
+					metric.External.Metric.Name, hpa.Spec.Metrics[correctNum].External.Target.AverageValue.String(), metric.External.Current.AverageValue.String())
+
+				// Convert to float64 using AsApproximateFloat64
+				currentAverageValueFloat := metric.External.Current.AverageValue.AsApproximateFloat64()
+				targetAverageValueFloat := hpa.Spec.Metrics[correctNum].External.Target.AverageValue.AsApproximateFloat64()
+
+				// Now you can perform calculations with these float64 values
+				if targetAverageValueFloat != 0 {
+					utilizationTemp := currentAverageValueFloat / targetAverageValueFloat
+					// Convert utilizationTemp to a percentage or format as needed
+					utilizationPercentage := utilizationTemp * 100 // Example to convert to percentage
+
+					// Now you can use utilizationPercentage safely
+					results = append(results, model.HPAUtilization{
+						MetricName:         metric.External.Metric.Name,
+						MetricType:         string(metric.Type),
+						HPAOwner:           hpa.Name,
+						ScaleTargetRefKind: hpa.Spec.ScaleTargetRef.Kind,
+						ScaleTargetRefName: hpa.Spec.ScaleTargetRef.Name,
+						AverageUtilization: utilizationPercentage,
+					})
+				} else {
+					// Handle the error scenario, maybe log that the target value is zero
+					log.Error("Target average value is zero")
+				}
+			}
+		}
+	}
+	return results, nil
+}
